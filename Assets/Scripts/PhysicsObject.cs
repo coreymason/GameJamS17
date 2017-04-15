cdd2ccd82ec8e26c8c9cdcfb00bc4eb2e97f3b2b
Stephen Ednave <LookASupernova@gmail.com>
2017-04-15 11:56:23 -0700

    Added Everything
    Yeah

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Move objects downward due to gravity
+public class PhysicsObject : MonoBehaviour
+{
+    [SerializeField] float gravityModifier = 1f;
+
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+
+    protected const float minMoveDistance = 0.001f;
+
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }   // OnEnable
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+
+    void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = Vector2.up * deltaPosition.y;
+
+        Movement(move);
+    }   // FixedUpdate
+
+
+
+    // Move the object
+    void Movement(Vector2 move)
+    {
+        float distance = move.magnitude;
+
+        rb2d.position = rb2d.position + move;
+    }   // Movement
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float jumpSpeed = 1.0f;
+    [SerializeField] private float gravityScale = 1.0f;
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if (Input.GetButtonDown("Jump"))
+        {
+
+        }
+	}
+}
